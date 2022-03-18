015f34bc487e4f2ad06e6747cc1e0bcf444f39e4
Arnoldblair <arnoldblair.munene@student.moringaschool.com>
2022-03-18 17:14:27 +0300

    add cloudinary

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import *
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 # Create your views here.
