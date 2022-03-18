a5b869cdea41d759fa02d2777ee994e07a21c9e4
Arnoldblair <arnoldblair.munene@student.moringaschool.com>
2022-03-18 16:50:42 +0300

    add category model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from datetime import datetime as dt
+from django.contrib.auth.models import User
+
+
+#cloudinary
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+# category model
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    # slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField(max_length=10000, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
