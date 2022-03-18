aa79237448dc2ae5a060abb1faea06643e7e36f6
Arnoldblair <arnoldblair.munene@student.moringaschool.com>
2022-03-18 17:17:39 +0300

    add homepage view

--- app/views.py
+++ app/views.py
@@ -6,3 +6,30 @@ import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 # Create your views here.
+
+
+# index view
+@login_required(login_url='/accounts/login/')
+def index(request):
+    # get current user
+    current_user = request.user
+    # get current user neighbourhood
+    profile = Profile.objects.filter(user_id=current_user.id).first()
+    # check if user has neighbourhood
+    if profile is None:
+        profile = Profile.objects.filter(
+            user_id=current_user.id).first()  # get profile
+        posts = Post.objects.filter(user_id=current_user.id)
+        # get all locations
+        locations = Location.objects.all()
+        neighbourhood = NeighbourHood.objects.all()
+        category = Category.objects.all()
+        businesses = Business.objects.filter(user_id=current_user.id)
+        contacts = Contact.objects.filter(user_id=current_user.id)
+        # redirect to profile with error message
+        return render(request, "profile.html", {"danger": "Update Profile by selecting Your Neighbourhood name to continue 😥!!", "locations": locations, "neighbourhood": neighbourhood, "categories": category, "businesses": businesses, "contacts": contacts, "posts": posts})
+    else:
+        neighbourhood = profile.neighbourhood
+        # get all posts in the neighbourhood of the user ordered by date
+        posts = Post.objects.filter(neighbourhood=neighbourhood).order_by("-created_at")
+        return render(request, 'index.html', {'posts': posts})
