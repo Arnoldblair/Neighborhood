4647aeb2b841ca44e72365542d6944c49fc11046
Arnoldblair <arnoldblair.munene@student.moringaschool.com>
2022-03-18 17:04:25 +0300

    add profile class model

--- app/models.py
+++ app/models.py
@@ -69,3 +69,23 @@ class NeighbourHood(models.Model):
 
     def __str__(self):
         return self.name        
+
+
+
+
+        # profile class model also 'user' as they collide with django user model
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=1)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100, blank=True, null=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.CASCADE)
+    profile_pic = CloudinaryField('image')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save_profile(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
