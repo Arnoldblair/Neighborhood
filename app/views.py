9b4539546d5ecac6e87dd2ca31e2aa02e562a7df
Arnoldblair <arnoldblair.munene@student.moringaschool.com>
2022-03-18 17:35:12 +0300

    add contact page views

--- app/views.py
+++ app/views.py
@@ -393,3 +393,31 @@ def business(request):
         businesses = Business.objects.filter(
             neighbourhood=profile.neighbourhood)
         return render(request, "business.html", {"businesses": businesses})
+
+
+# contact page
+@login_required(login_url="/accounts/login/")
+def contacts(request):
+    current_user = request.user
+    # get current user neighbourhood
+    profile = Profile.objects.filter(user_id=current_user.id).first()
+    # check if user has neighbourhood
+    if profile is None:
+        profile = Profile.objects.filter(
+            user_id=current_user.id).first()  # get profile
+        posts = Post.objects.filter(user_id=current_user.id)
+        # get all locations
+        locations = Location.objects.all()
+        neighbourhood = NeighbourHood.objects.all()
+        category = Category.objects.all()
+        businesses = Business.objects.filter(user_id=current_user.id)
+        contacts = Contact.objects.filter(user_id=current_user.id)
+        # redirect to profile with error message
+        return render(request, "profile.html", {"danger": "Update Profile by selecting Your Neighbourhood name to continue 😥!!", "locations": locations, "neighbourhood": neighbourhood, "categories": category, "businesses": businesses, "contacts": contacts, "posts": posts})
+    else:
+        neighbourhood = profile.neighbourhood
+        # get all contacts where the neighbourhood is the same as the user neighbourhood
+        contacts = Contact.objects.filter(
+            neighbourhood=profile.neighbourhood).order_by("-created_at")
+        return render(request, "contacts.html", {"contacts": contacts, "neighbourhood": profile.neighbourhood})
+
