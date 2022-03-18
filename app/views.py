9705e280f6730997e244de61a68513a0436fb6d0
Arnoldblair <arnoldblair.munene@student.moringaschool.com>
2022-03-18 17:32:44 +0300

    update views

--- app/views.py
+++ app/views.py
@@ -336,3 +336,33 @@ def posts(request):
         # get all posts in the neighbourhood of the user ordered by date
         posts = Post.objects.filter(neighbourhood=neighbourhood).order_by("-created_at")
         return render(request, "posts.html", {"posts": posts})
+
+
+@login_required(login_url="/accounts/login/")
+# alerts page
+def alerts(request):
+    current_user = request.user
+    # get current user neighbourhood
+    profile = Profile.objects.filter(user_id=current_user.id).first()
+    # check if user has neighbourhood
+    if profile is None:
+        profile = Profile.objects.filter(
+            user_id=current_user.id).first()  # get profile
+        posts = Post.objects.filter(user_id=current_user.id)
+        # get all locations
+        locations = Location.objects.all()
+        neighbourhood = NeighbourHood.objects.all()
+        category = Category.objects.all()
+        businesses = Business.objects.filter(user_id=current_user.id)
+        contacts = Contact.objects.filter(user_id=current_user.id)
+        # redirect to profile with error message
+        return render(request, "profile.html", {"danger": "Update Profile by selecting Your Neighbourhood name to continue 😥!!", "locations": locations, "neighbourhood": neighbourhood, "categories": category, "businesses": businesses, "contacts": contacts, "posts": posts})
+    else:
+        neighbourhood = profile.neighbourhood
+        # get the category that contains name "alerts"
+        category = Category.objects.get(name="alerts")
+        # get all posts that contains the word "alert" and order by date within the neighbourhood of the user
+        posts = Post.objects.filter(
+            neighbourhood=neighbourhood, category=category).order_by("-created_at")
+        return render(request, "alerts.html", {"posts": posts})
+
