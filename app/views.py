dedfa97fddc6692897db28bd1549ce173b1f10f6
Arnoldblair <arnoldblair.munene@student.moringaschool.com>
2022-03-18 17:33:58 +0300

    add business page views

--- app/views.py
+++ app/views.py
@@ -366,3 +366,30 @@ def alerts(request):
             neighbourhood=neighbourhood, category=category).order_by("-created_at")
         return render(request, "alerts.html", {"posts": posts})
 
+
+
+# business page
+@login_required(login_url="/accounts/login/")
+def business(request):
+    current_user = request.user
+    # get current user neighbourhood
+    profile = Profile.objects.filter(user_id=current_user.id).first()
+    # check if user has neighbourhood
+    if profile is None:
+        profile = Profile.objects.filter(
+            user_id=current_user.id).first()  # get profile
+        posts = Post.objects.filter(user_id=current_user.id)
+        # get all locations
+        locations = Location.objects.all()
+        neighbourhood = NeighbourHood.objects.all()
+        category = Category.objects.all()
+        businesses = Business.objects.filter(user_id=current_user.id)
+        contacts = Contact.objects.filter(user_id=current_user.id)
+        # redirect to profile with error message
+        return render(request, "profile.html", {"danger": "Update Profile by selecting Your Neighbourhood name to continue 😥!!", "locations": locations, "neighbourhood": neighbourhood, "categories": category, "businesses": businesses, "contacts": contacts, "posts": posts})
+    else:
+        neighbourhood = profile.neighbourhood
+        # get all businesses in the user neighbourhood
+        businesses = Business.objects.filter(
+            neighbourhood=profile.neighbourhood)
+        return render(request, "business.html", {"businesses": businesses})
